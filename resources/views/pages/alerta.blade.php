9937436a5609142df87afe3119c9baee14da6a9f
Katatau1 <andersonrunguefilho@gmail.com>
2024-06-25 20:55:03 -0300

    FAlhando misseravelmente em html ;c

---
+++ resources/views/pages/alerta.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('conteudo')
+    <form action="/criacao" method="post">
+        @csrf
+
+        <Div>Alerta!</Div>
+        <label for="excluir"> Tem certeza que deseja excluir?</label>
+        
+        <button id="sim" type="submit">SIM</button>    <button id="nao" type="submit">NÂO</button>   
+
+    </form>
+
+    @endsection
---
+++ resources/views/pages/criar.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('conteudo')
+    <form action="/criacao" method="post">
+        @csrf
+
+        <Header>
+            Brawlist
+        </Header>
+        <Div>
+            <h1>
+                Criação
+            </h1>
+        <p>
+            <label for="palavra">Nome</label>
+            <input type="text" id="nome" name="nome" value="">
+        </p>
+
+        <p>
+            <label for="titulo">Titulo</label>
+            <input type="text" id="titulo" name="titulo" value="">
+        </p>
+        <p>
+            <label for="imagem">Url da imagem</label>
+            <input type="text" id="urlimagem" name="imagem" value="">
+        </p>
+
+        <p>
+            <label for="raridade">Raridade</label>
+            <select id="raridade" name="raridade">
+                <option value="Raro">Raro</option>
+                <option value="Super-Raro">Super-Raro</option>
+                <option value="Épico">Épico</option>
+                <option value="Mítico">Mítico</option>
+                <option value="Lendário">Lendário</option>
+            </select>
+        </p>
+
+        <p>
+            <label for="categoria">Categoria</label>
+            <select id="categoria" name="categoria">
+                <option value="Destruidor">Destruidor</option>
+                <option value="Algoz">Algoz</option>
+                <option value="Tiro-Preciso">Tiro-Preciso</option>
+                <option value="Detonador">Detonador</option>
+                <option value="Tanque">Tanque</option>
+                <option value="Suporte">Suporte</option>
+                <option value="Controle">Controle</option>
+            </select>
+        </p>
+
+       
+        <p>
+            <button type="submit">Criar</button>    
+        </p>
+        
+    </Div>
+
+        <footer>
+            Feito Pelos Cotemigos
+        </footer>
+    </form>
+
+    @endsection
