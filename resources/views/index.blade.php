3ca0b3eebbf2016db8cdfe490de98dcd46d349b3
DaviGroppo <106624033+DaviGroppo@users.noreply.github.com>
2024-06-26 20:31:48 -0400

    tentando

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 <a href='/create'>Criar</a>
 <div>
     @foreach($brawlers as $brawler)
+    <Header> Mostrar Brawlers</Header>
         <div>
             <h1>{{ $brawler->nome }} </h1>
             <h2>{{ $brawler->titulo }} </h2>
@@ -12,6 +13,7 @@
             <p>{{ $brawler->raridade}}</p>
             <a href="/edit/{{$brawler->id}}">Editar</a>
         </div>
+        <footer>Feito pelos Cotemigos</footer>
     @endforeach
 </div>
 @endsection
