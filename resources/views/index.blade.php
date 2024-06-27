70933fbec3cf69c8846ab8b25b5c199a8b1ae5c8
DaviGroppo <106624033+DaviGroppo@users.noreply.github.com>
2024-06-26 20:36:47 -0400

    t

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
 <a href='/create'>Criar</a>
 <div>
     @foreach($brawlers as $brawler)
-    <Header> Mostrar Brawlers</Header>
+    <H1> Mostrar Brawlers</H1>
         <div>
-            <h1>{{ $brawler->nome }} </h1>
-            <h2>{{ $brawler->titulo }} </h2>
+            <h2>{{ $brawler->nome }} </h2>
+            <h3>{{ $brawler->titulo }} </h3>
             <img width="200px" src="{{$brawler->imagem}}">
             <p>{{ $brawler->categoria}}</p>
             <p>{{ $brawler->raridade}}</p>
